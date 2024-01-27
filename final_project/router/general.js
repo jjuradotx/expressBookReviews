22b1f0126248e4620ebd2d0b2938e4a6fd445f80
JJurado <JJurado@gmail.com>
2024-01-27 04:10:25 -0500

    New saves

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,17 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    return res.send(JSON.stringify(books[isbn],null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  //return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books[author],null,4))
 });
 
 // Get all books based on title
