64941999b4f0a98107f547fb6403215c5621ae41
JJurado <JJurado@gmail.com>
2024-01-28 08:21:01 -0500

    New saves

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,7 +57,9 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  //return res.send("books by review:" + JSON.stringify(books[isbn].review,null,4))
+  return res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
